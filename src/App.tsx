8219a988f2ee57ae871d4b74d41476a40202241a
Tunahan Bayındır <tbayndr@yahoo.com>
2024-08-29 15:01:40 +0200

    refactor: update the steps

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,17 @@ const car = {
 
 /**
  * --- Reminder ---
- * Please mind the file and folder structure.
- * You can create new files or folders.
+ * - Please mind the file and folder structure. You can create new files or folders.
+ * - You can use official React documentation.
  * 
  * --- Instructions ---
  * Create a form component that renders a form based on the config object.
+ *  - Render the form elements based on the config object.
+ *  - You don't need to have a default value for the fields. The ones in the screenshots are just examples.
+ *  - You can use either controlled or uncontrolled inputs.
+ * 
  * When the form is submitted:
- * - Check if all fields are valid.
+ * - Check if all fields are valid. You'll need to validate the fields based on the configuration you will create.
  * - If all fields are valid, log all form elements to the console.
  * - If any field is invalid, display an error message.
  * 
@@ -29,6 +33,7 @@ const car = {
  * - number
  * - boolean
  * - enum
+ * 
  */
 function App() {
   return (
